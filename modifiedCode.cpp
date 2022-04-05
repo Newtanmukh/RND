f9c13e8aeae73fd0dc8a44b453c08c3335818229
Arun Bhat <arunkonebhat@gmail.com>
2022-04-05 09:27:47 +0530

    error resolved - 1

---
+++ modifiedCode.cpp
@@ -0,0 +1,135 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// this is basically the node of each block.
+// This is the data structure for the basic block of a CFG.
+
+struct Node
+{
+  int id;
+  vector<string> store;
+  Node *left;
+  Node *right;
+  // constructor
+  Node(int x)
+  {
+    id = x;
+    left = NULL;
+    right = NULL;
+  }
+};
+
+void dfs(Node *node, vector<string> local, vector<int> checknode, vector<vector<string> > &all_paths)
+{
+  if (node == NULL)
+    return;
+
+  // checking if the node had already been travelled once in this particular traversal.
+  if (count(checknode.begin(), checknode.end(), node->id))
+  {
+    return;
+  }
+
+  // visited
+  checknode.push_back(node->id);
+
+  // leaf node
+  if (node->left == NULL && node->right == NULL)
+  {
+    all_paths.push_back(node->store);
+    return;
+  }
+
+  // for(auto &x : node->store)
+  //   {
+  //       local.push_back(x);
+  //   }
+  local.insert(local.end(), node->store.begin(), node->store.end());
+
+  dfs(node->left, local, checknode, all_paths);
+  dfs(node->right, local, checknode, all_paths);
+}
+
+int main()
+{
+  vector<vector<string> > all_paths;
+  // code for extracting CFG.
+  //*****************************
+  ifstream inFile;
+  inFile.open("blocks.txt");
+
+  vector<string> orderdata;
+  map<int, Node *> mapper;
+
+  while (true)
+  {
+    string str;
+    inFile >> str;
+    if (inFile.eof())
+      break;
+    orderdata.push_back(str);
+  }
+
+  for (int i = 0; i < orderdata.size(); i++)
+  {
+    if (orderdata[i][0] >= '0' && orderdata[i][0] <= '9')
+    {
+      int blocknumber = stoi(orderdata[i]);
+      int j = i + 1;
+      vector<string> blockopcodes;
+      while (orderdata[j][0] < '0' || orderdata[j][0] > '9')
+      {
+        blockopcodes.push_back(orderdata[j]);
+        j++;
+      }
+
+      Node *newnode = new Node(blocknumber);
+
+      // for (auto &x : blockopcodes)
+      // {
+      //   newnode->store.push_back(x);
+      // }
+      newnode->store.insert(newnode->store.end(), blockopcodes.begin(), blockopcodes.end());
+
+      mapper[blocknumber] = newnode;
+    }
+    else
+    {
+      continue;
+    }
+  }
+
+  //*****************************
+
+  vector<int> checknodelocal;
+  vector<string> opcodesofeachloop;
+
+  /*
+    for(auto x:mapper)
+      {
+        cout<<x.first<<endl;
+        for(auto p:x.second->store)
+          {
+           cout<<p<<endl;
+          }
+      }*/
+
+  // dfs(node,opcodesofeachloop,checknodelocal);
+
+  map<pair<string, pair<string, string> >, int> frequency;
+
+  int i;
+
+  // for(auto &each_path : all_paths)
+  //{
+  // for(i=0;i<each_path.size()-2;i++)
+  //{
+  // to store the frequency of each 3 - gram
+  // frequency[make_pair(each_path[i],make_pair(each_path[i+1],each_path[i+1]))]++;
+  //}
+  //}
+
+  // further write code to extract uniquee/most occuring trigrams.
+}
+
+//     SVC(kernel='linear', C = 1)
